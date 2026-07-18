6f5ffc89376610322879b92788f472f5fe2bae89
Brian Kareithi <kareithibrian1@gmail.com>
2026-07-18 16:15:30 +0300

    modernize navbar: sticky, responsive, hamburger menu

--- src/Components/Navbar/navbar.component.jsx
+++ src/Components/Navbar/navbar.component.jsx
@@ -1,53 +1,52 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import Searchbox from "./Icons/search-box/search-box.component";
 import Account from "./Icons/account-icon/account.component";
 import Cart from "./Icons/cart-icon/cart.component";
 import Wish from "./Icons/wishlist-icon/wishlist.component";
-import  './navbar.style.css';
-class Navbar extends Component
-{
-    render() {
-        return(
-            <nav>
-                <h1 className={'text01'}>Crafted Sol</h1>
-                <div className={'display01'}>
-                <Searchbox/>
-                    <div className={'items01'}>
-                <div>
-                    <h1>Men</h1>
-                </div>
+import './navbar.style.css';
 
-                <div>
-                    <h1>Women</h1>
-                </div>
+const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
 
-                <div>
-                    <h1>Shoes</h1>
-                </div>
+  return (
+    <nav className="navbar">
+      <div className="navbar__inner">
+        <div className="navbar__logo">
+          <h1>Crafted Sol</h1>
+        </div>
 
-                <div>
-                    <h1>Bags</h1>
-                </div>
+        <button
+          className={`navbar__hamburger ${menuOpen ? 'open' : ''}`}
+          onClick={() => setMenuOpen(!menuOpen)}
+          aria-label="Toggle menu"
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </button>
 
-                <div>
-                    <h1>Afri Culture</h1>
-                </div>
+        <div className={`navbar__center ${menuOpen ? 'navbar__center--open' : ''}`}>
+          <div className="navbar__search">
+            <Searchbox />
+          </div>
+          <div className="navbar__links">
+            <a href="#men">Men</a>
+            <a href="#women">Women</a>
+            <a href="#shoes">Shoes</a>
+            <a href="#bags">Bags</a>
+            <a href="#afri-culture">Afri Culture</a>
+            <a href="#signatures">Signatures</a>
+          </div>
+        </div>
 
-                <div>
-                    <h1>Signatures</h1>
-                </div>
-                    </div>
-
-                    <div className={'items02'}>
-                <Wish/>
-                <Cart/>
-                <Account/>
-                    </div>
-                </div>
-
-            </nav>
-        );
-    }
-}
+        <div className="navbar__icons">
+          <Wish />
+          <Cart />
+          <Account />
+        </div>
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
